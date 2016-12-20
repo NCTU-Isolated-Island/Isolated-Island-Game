9317c9fb9a628576015809ab985e779be605b5b9
Steven <karta1297963@gmail.com>
2016-12-20 21:03:18 +0800

    no message

---
+++ UnityProject/Assets/Steven/Architecture.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Architecture : MonoBehaviour {
+
+	void Awake()
+	{
+		
+
+
+		// Player Login 
+		// if (first login) 
+		// Register
+
+
+
+	}
+
+	void Register()
+	{
+		//Register
+
+		//Login
+	}
+
+
+	void OnLogin()
+	{
+		// switch to MainScene
+	}
+
+	void OnSwitchMainScene(){
+		
+
+		//InstantiateUserGameObject
+
+
+		//設定完之後再把顯示出遊戲畫面（在這之前可能顯示loading bar
+	}
+
+	//5秒更新一次的Update
+	void SlowUpdate()
+	{
+		//Send my locationInfo;
+	}
+
+	void OnUserLocationChange()
+	{
+		//改變對應的User位置
+	}
+
+	void OnUserPlaceElement()
+	{
+		//顯示該Element
+	}
+
+}
+
+public class PlayerController : MonoBehaviour {
+
+	void Update()
+	{
+		//if 連點兩下User or Player 
+		// Zoom in
+
+		//if 點選場上的Furnace
+		//(Enable and)Slide in Furnace UICanvas
+	}
+
+	void ChangeViewingAngle()
+	{
+		
+	}
+
+	void PlaceElement()
+	{
+		
+	}
+
+	//融合場景的Code
+}
+
+//Attach under MainCamera
+public class CameraController : MonoBehaviour {
+
+	//放大（查看）Player or User 的船
+	void ZoomIn(){
+		
+	}
+
+	//回到鳥瞰視角
+	void ZoomOut(){
+		
+	}
+
+
+}
---
+++ UnityProject/Assets/Steven/LocationSystem.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class LocationSystem : MonoBehaviour {
+
+	public static LocationSystem Instance;
+
+	private System.DateTime updateTime;
+	private Vector3 previousLocation;
+
+	void Awake()
+	{
+		if(Instance == null){
+			Instance = this;
+		}
+
+
+	}
+
+	void Update()
+	{	
+		if(updateTime != ConvertFromUnixTimestamp(Input.location.lastData.timestamp))
+		{
+			
+			transform.rotation = Quaternion.LookRotation(GetInGameCoordinate() - previousLocation);
+			
+			GameManager.Instance.OnPlayerLocationChange
+			(
+				GetInGameCoordinate(),
+				transform.eulerAngles.y
+			);
+
+			previousLocation = GetInGameCoordinate();
+			updateTime = ConvertFromUnixTimestamp(Input.location.lastData.timestamp);
+		}
+
+	}
+
+	public void StartLocationService()
+	{
+		StartCoroutine(TryStartLocationService());
+	}
+
+	public void StopLocationService()
+	{
+		Input.location.Stop();
+	}
+
+//	private void UpdateLocationInfo()
+//	{
+//		locationInfo.text =
+//			"Altitude: " + Input.location.lastData.altitude +
+//			" Vertical Accuracy: " + Input.location.lastData.verticalAccuracy +
+//			"\nLatitude: " + Input.location.lastData.latitude +
+//			" Longitude: " + Input.location.lastData.longitude +
+//			" HorizontalAccuracy: " + Input.location.lastData.horizontalAccuracy + 
+//			"\nTimeStamp: " + ConvertFromUnixTimestamp(Input.location.lastData.timestamp).ToLongTimeString();
+//		;
+//	}
+
+	//Longitude 經度 1 = 110KM
+	//Latitude 緯度 1 = 100KM
+
+	public Vector3 GetInGameCoordinate()
+	{
+		//FOR DEVELOP
+		if(Input.location.status != LocationServiceStatus.Running)
+		{
+			return Vector3.one;
+		}
+
+		Vector3 result;
+		result.x = (Input.location.lastData.longitude - 120.997325f) * 110000;
+		result.y = 0f;
+		result.z = (Input.location.lastData.latitude - 24.787493f) * 100000;
+		print(result);
+		return result;
+	}
+
+	private IEnumerator TryStartLocationService()
+	{	
+		// First, check if user has location service enabled
+		if (!Input.location.isEnabledByUser)
+		{
+			print("Location Service not enabled");
+			yield break;
+		}
+
+		// Start service before querying location
+		Input.location.Start();
+
+		// Wait until service initializes
+		int maxWait = 20;
+		while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+		{	
+			yield return new WaitForSeconds(1);
+			maxWait--;
+		}
+
+		// Service didn't initialize in 20 seconds
+		if (maxWait < 1)
+		{
+			print("Timed out");
+			yield break;
+		}
+
+		// Connection has failed
+		if (Input.location.status == LocationServiceStatus.Failed)
+		{
+			print("Unable to determine device location");
+			yield break;
+		}
+
+	}
+
+	public static System.DateTime ConvertFromUnixTimestamp(double timestamp)
+	{
+		System.DateTime origin = new System.DateTime(1970, 1, 1, 0, 0, 0, 0);
+		return origin.AddSeconds(timestamp).AddHours(8);
+	}
+}
