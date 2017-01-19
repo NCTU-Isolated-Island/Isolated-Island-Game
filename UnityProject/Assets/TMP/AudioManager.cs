3adde4254494667f93220980acde6b0d20b66b4a
Steven's MacBook <Cyric@CyricdeMacBook.local>
2017-01-19 15:53:53 +0800

    Add: Ocean mesh

---
+++ UnityProject/Assets/TMP/AudioManager.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.Audio;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+    public static AudioManager Instance { get; private set; }
+
+    [SerializeField]
+    private AudioSource [] AudioPlayer;
+    public float MasterVolume;
+
+    private int UsingPlayer; // 0 means using player1 , 1 means using player2
+    private bool Fading;
+
+    void Awake()
+    {
+        foreach (AudioSource player in AudioPlayer)
+        {
+            player.volume = 0;
+            player.loop = true;
+        }
+        Instance = this;
+        UsingPlayer = 0;
+        Fading = false;
+        MasterVolume = 1;
+    }
+
+    void ChangeUsingPlayer()
+    {
+        UsingPlayer = 1 - UsingPlayer;
+    }
+
+    bool SetVolume(float new_volume) // return success or not
+    {
+        if(Fading == true) return false;
+
+        AudioPlayer[UsingPlayer].volume = new_volume;
+
+        return true;
+    }
+
+    public bool SetBGM(string SourceName)
+    {
+        //
+        print(SourceName);
+        //
+        AudioClip new_clip = AssetDatabase.LoadAssetAtPath("Assets/Resources/Audio/" + SourceName , typeof(AudioClip)) as AudioClip;
+        // can rewrite to (SourceName , FolderName)
+
+        ChangeUsingPlayer();
+        AudioPlayer[UsingPlayer].clip = new_clip;
+        StartCoroutine(FadeMusic(5));
+
+        AudioPlayer[UsingPlayer].Play();
+        return true;
+    }
+
+    IEnumerator FadeMusic(float OverTime)
+    {
+        Fading = true;
+
+        float pass_time = 0;
+        float IngVolume = AudioPlayer[1 - UsingPlayer].volume;
+
+        while (pass_time < OverTime)
+        {
+            AudioPlayer[UsingPlayer].volume = Mathf.Lerp(0, MasterVolume, pass_time / OverTime);
+            AudioPlayer[1 - UsingPlayer].volume = Mathf.Lerp(IngVolume, 0, pass_time / OverTime);
+            pass_time += Time.deltaTime;
+            yield return null;
+        }
+        AudioPlayer[UsingPlayer].volume = MasterVolume;
+        AudioPlayer[1 - UsingPlayer].volume = 0;
+
+        Fading = false;
+    }
+
+    //
+
+    void Start()
+    {
+        GameManager.Instance.PlayerGameObject.transform.Find("ShipModel").Find("oil_tanker").gameObject.AddComponent<BGMController>();
+        GameManager.Instance.PlayerGameObject.transform.Find("ShipModel").Find("oil_tanker").gameObject.AddComponent<Rigidbody>();
+        GameManager.Instance.PlayerGameObject.transform.Find("ShipModel").Find("oil_tanker").gameObject.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    //
+}
---
+++ UnityProject/Assets/TMP/BGMController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BGMController : MonoBehaviour {
+
+    private bool Exited; // true if already exit previous
+    private Dictionary<string, string> BGMDictionary;
+
+    private string PrevBGMpath;
+
+	void OnTriggerStay(Collider EnterOcean)
+    {
+        //
+        print(EnterOcean.name);
+        //
+        if (Exited == false) return;
+
+        string nextBGMpath = BGMDictionary[EnterOcean.name];
+
+        if (nextBGMpath == PrevBGMpath) return;
+        PrevBGMpath = nextBGMpath;
+
+        AudioManager.Instance.SetBGM(nextBGMpath);
+        Exited = false;
+    }
+
+    void OnTriggerExit(Collider ExitOcean)
+    {
+        Exited = true;
+    }
+
+    void Awake()
+    {
+        BGMDictionary = new Dictionary<string, string>();
+    }
+
+    void Start() // for testing
+    {
+        BGMDictionary["a_girl_from_osaka_1"] = "Sound1.mp3";
+        BGMDictionary["a_new_generation_1"] = "Sound2.mp3";
+
+        Exited = true;
+    }
+}
