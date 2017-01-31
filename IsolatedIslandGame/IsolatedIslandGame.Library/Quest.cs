54341c7623a58de695c46c88379d25db1ac9170a
DSobscure <snbruce13@yahoo.com.tw>
2017-01-31 18:38:52 +0800

    initial

---
+++ IsolatedIslandGame/IsolatedIslandGame.Library/Quest.cs
@@ -0,0 +1,12 @@
+﻿namespace IsolatedIslandGame.Library
+{
+    public abstract class Quest
+    {
+        public int QuestID { get; private set; }
+
+        protected Quest(int questID)
+        {
+            QuestID = questID;
+        }
+    }
+}
