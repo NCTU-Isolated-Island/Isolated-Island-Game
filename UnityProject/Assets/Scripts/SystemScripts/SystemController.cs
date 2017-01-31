b0db11303bfb66e24978adcabf2117ab91a2c068
DSobscure <snbruce13@yahoo.com.tw>
2017-01-31 17:30:59 +0800

    resolve conflict

---
+++ UnityProject/Assets/Scripts/SystemScripts/SystemController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using IsolatedIslandGame.Library;
+
+namespace IsolatedIslandGame.Client.Scripts.SystemScripts
+{
+    public class SystemController : MonoBehaviour
+    {
+        private void Awake()
+        {
+            UserManager.Initial(new ClientUserManager());
+            ClientSystemManager.Initial(UserManager.Instance.User);
+            ItemManager.Initial(new ClientItemManager());
+            VesselManager.Initial(new ClientVesselManager());
+            BlueprintManager.Initial(new ClientBlueprintManager());
+            PlayerInformationManager.Initial(new PlayerInformationManager());
+        }
+    }
+}
