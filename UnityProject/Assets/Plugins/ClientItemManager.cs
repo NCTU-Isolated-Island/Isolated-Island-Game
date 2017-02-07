33c018c62b6bda21b37329d7ddae603a63b96612
DSobscure <snbruce13@yahoo.com.tw>
2017-02-07 20:35:39 +0800

    client bug fixed

--- UnityProject/Assets/Plugins/ClientItemManager.cs
+++ UnityProject/Assets/Plugins/ClientItemManager.cs
@@ -11,7 +11,13 @@ namespace IsolatedIslandGame.Client
 
         public ClientItemManager()
         {
-            SystemManager.Instance.OperationManager.FetchDataResolver.FetchAllItems();
+            Communication.PhotonService.Instance.OnConnectChange += (connected) =>
+            {
+                if(connected)
+                {
+                    SystemManager.Instance.OperationManager.FetchDataResolver.FetchAllItems();
+                }
+            };
         }
 
         public override void AddItem(Item item)
