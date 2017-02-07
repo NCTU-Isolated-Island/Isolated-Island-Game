ccd2e823ce3391ba42179f85624676217117b7ad
DSobscure <snbruce13@yahoo.com.tw>
2017-02-07 22:20:35 +0800

    Change Directory

---
+++ UnityProject/Assets/Scripts/UI_Scripts/BAG/IB_BackButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class IB_BackButton : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject Bag;
+    // Use this for initialization
+    void Start () {
+	
+	}
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Bag.GetComponent<ShowBag_pos>().UpAndDown();
+    }
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
---
+++ UnityProject/Assets/Scripts/UI_Scripts/COMBINE/Combine_block.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Combine_block : MonoBehaviour,IPointerClickHandler {
+
+    public GameObject UIControl;
+    public GameObject Bag;
+    public int ItemInHere;
+    // Use this for initialization
+    void Start () {
+        if (!UIControl)
+        {
+            UIControl = GameObject.FindWithTag("UImanager");
+        }
+        if (!Bag)
+        {
+            Bag = UIControl.GetComponent<UImanager>().Bag;
+        }
+    }
+	public void OnPointerClick(PointerEventData eventData)
+    {       
+        Bag.GetComponent<ShowBag_pos>().CombineArea = this.gameObject;
+        Bag.GetComponent<ShowBag_pos>().UpAndDown();
+    }
+
+}
