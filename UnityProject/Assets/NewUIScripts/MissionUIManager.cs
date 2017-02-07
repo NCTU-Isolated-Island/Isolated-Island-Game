434bb4d2365816892aa0842fabe598d09fcdd257
Alan Shih (IS-TW-INTRN) <alan_shih@trend.com.tw>
2017-02-08 01:47:26 +0800

    20170208 - Second
    New - LExpand - Finish LExpand Button (Script and Prefab)

---
+++ UnityProject/Assets/NewUIScripts/MissionUIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MissionUIManager : MonoBehaviour {
+    
+    public GameObject MissionDetailPanel;
+    int Page = 1;
+    public GameObject Page1, Page2;
+    public Button Right, Left;
+    public GameObject MissionSet;
+    public GameObject MissionContent;
+
+    public Button View_Button;
+    public Button Back_Button;
+
+    public Sprite[] Pic_ICON;
+
+    public Button ListButton;
+    public GameObject FunctionList;
+
+    public float CanvasWidth ,  CanvasHeight;
+}
---
+++ UnityProject/Assets/TMP/LExpand.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LExpand : MonoBehaviour
+{
+    private bool ExpandOrWithdraw;
+
+    public GameObject viewportHorizontal;
+    public GameObject viewportVertical;
+
+    private float hori_ori;
+    private float ver_ori;
+
+    private IEnumerator coroutine;
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 horiTmp = viewportHorizontal.GetComponent<RectTransform>().offsetMin;
+        Vector3 verTmp = viewportVertical.GetComponent<RectTransform>().offsetMax;
+        hori_ori = horiTmp.x;
+        ver_ori = verTmp.y;
+
+        ExpandOrWithdraw = false;
+        // TESTING
+        //Invoke("ExpandBtn", 1f);
+        //Invoke("WithDrawBtn", 2f);
+    }
+
+    public void OnClick()
+    {
+        if (ExpandOrWithdraw) ExpandBtn();
+        else WithDrawBtn();
+
+        ExpandOrWithdraw = !ExpandOrWithdraw;
+    }
+
+    public void ExpandBtn()
+    {
+        if (coroutine != null) StopCoroutine(coroutine);
+        coroutine = ExpandBtnCoroutine(false);
+        StartCoroutine(coroutine);
+    }
+
+    public void WithDrawBtn()
+    {
+        if (coroutine != null) StopCoroutine(coroutine);
+        coroutine = ExpandBtnCoroutine(true);
+        StartCoroutine(coroutine);
+    }
+
+    IEnumerator ExpandBtnCoroutine(bool OnOff)
+    {
+        print("OnOff = " + OnOff);
+
+        float passTime = 0f;
+        Vector3 horiTmp = viewportHorizontal.GetComponent<RectTransform>().offsetMin;
+        Vector3 verTmp = viewportVertical.GetComponent<RectTransform>().offsetMax;
+
+        float hori_st = horiTmp.x;
+        float ver_st = verTmp.y;
+
+        while (passTime < 1.5f)
+        {
+            if (OnOff == true)
+            {
+                horiTmp.x = Mathf.Lerp(hori_st, 0, passTime / 1.5f);
+                viewportHorizontal.GetComponent<RectTransform>().offsetMin = horiTmp;
+                verTmp.y = Mathf.Lerp(ver_st, 0, passTime / 1.5f);
+                viewportVertical.GetComponent<RectTransform>().offsetMax = verTmp;
+            }
+            else
+            {
+                horiTmp.x = Mathf.Lerp(hori_st, hori_ori, passTime / 1.5f);
+                viewportHorizontal.GetComponent<RectTransform>().offsetMin = horiTmp;
+                verTmp.y = Mathf.Lerp(ver_st, ver_ori, passTime / 1.5f);
+                viewportVertical.GetComponent<RectTransform>().offsetMax = verTmp;
+            }
+
+            passTime += Time.deltaTime;
+            yield return null;
+        }
+    }
+
+}
