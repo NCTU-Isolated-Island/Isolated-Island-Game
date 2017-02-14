beaac92fdf97b9a5c0bb37636e36d511daf5b712
DSobscure <snbruce13@yahoo.com.tw>
2017-02-14 23:23:28 +0800

    landmark

---
+++ IsolatedIslandGame/IsolatedIslandGame.Library/Landmark/Landmark.cs
@@ -0,0 +1,50 @@
+﻿using IsolatedIslandGame.Protocol;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IsolatedIslandGame.Library.Landmark
+{
+    public class Landmark
+    {
+        public int LandmarkID { get; private set; }
+        public string LandmarkName { get; private set; }
+
+        private Dictionary<int, LandmarkRoom> roomDictionary = new Dictionary<int, LandmarkRoom>();
+        public IEnumerable<LandmarkRoom> Rooms { get { return roomDictionary.Values.ToArray(); } }
+
+        public delegate void LandmarkRoomChangeEventHandler(DataChangeType changeType, LandmarkRoom room);
+        private event LandmarkRoomChangeEventHandler onLandmarkRoomChange;
+        public event LandmarkRoomChangeEventHandler OnLandmarkRoomChange { add { onLandmarkRoomChange += value; } remove { onLandmarkRoomChange -= value; } }
+
+        public bool ContainsRoom(int roomID)
+        {
+            return roomDictionary.ContainsKey(roomID);
+        }
+        public bool FindRoom(int roomID, out LandmarkRoom room)
+        {
+            return roomDictionary.TryGetValue(roomID, out room);
+        }
+        public void LoadRoom(LandmarkRoom room)
+        {
+            if(ContainsRoom(room.LandmarkRoomID))
+            {
+                roomDictionary[room.LandmarkRoomID] = room;
+                onLandmarkRoomChange?.Invoke(DataChangeType.Update, room);
+            }
+            else
+            {
+                roomDictionary.Add(room.LandmarkRoomID, room);
+                onLandmarkRoomChange?.Invoke(DataChangeType.Add, room);
+            }
+        }
+        public void RemoveRoom(int roomID)
+        {
+            if(ContainsRoom(roomID))
+            {
+                LandmarkRoom room = roomDictionary[roomID];
+                roomDictionary.Remove(roomID);
+                onLandmarkRoomChange?.Invoke(DataChangeType.Remove, room);
+            }
+        }
+    }
+}
---
+++ IsolatedIslandGame/IsolatedIslandGame.Library/Landmark/LandmarkRoom.cs
@@ -0,0 +1,50 @@
+﻿using IsolatedIslandGame.Protocol;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IsolatedIslandGame.Library.Landmark
+{
+    public class LandmarkRoom
+    {
+        public int LandmarkRoomID { get; private set; }
+        public string RoomName { get; private set; }
+        public int HostPlayerID { get; private set; }
+        private Dictionary<int, MutiplayerSynthesizeParticipationInfo> mutiplayerSynthesizeParticipationInfoDictionary = new Dictionary<int, MutiplayerSynthesizeParticipationInfo>();
+        public IEnumerable<MutiplayerSynthesizeParticipationInfo> MutiplayerSynthesizeParticipationInfos { get { return mutiplayerSynthesizeParticipationInfoDictionary.Values.ToArray(); } }
+
+        public delegate void MutiplayerSynthesizeParticipationInfoChangeEventHandler(DataChangeType changeType, MutiplayerSynthesizeParticipationInfo info);
+        private event MutiplayerSynthesizeParticipationInfoChangeEventHandler onMutiplayerSynthesizeParticipationInfoChange;
+        public event MutiplayerSynthesizeParticipationInfoChangeEventHandler OnMutiplayerSynthesizeParticipationInfoChange { add { onMutiplayerSynthesizeParticipationInfoChange += value; } remove { onMutiplayerSynthesizeParticipationInfoChange -= value; } }
+
+        public bool ContainsMutiplayerSynthesizeParticipant(int playerID)
+        {
+            return mutiplayerSynthesizeParticipationInfoDictionary.ContainsKey(playerID);
+        }
+        public bool FindMutiplayerSynthesizeParticipationInfo(int playerID, out MutiplayerSynthesizeParticipationInfo info)
+        {
+            return mutiplayerSynthesizeParticipationInfoDictionary.TryGetValue(playerID, out info);
+        }
+        public void LoadMutiplayerSynthesizeParticipationInfo(MutiplayerSynthesizeParticipationInfo info)
+        {
+            if (ContainsMutiplayerSynthesizeParticipant(info.playerID))
+            {
+                mutiplayerSynthesizeParticipationInfoDictionary[info.playerID] = info;
+                onMutiplayerSynthesizeParticipationInfoChange?.Invoke(DataChangeType.Update, info);
+            }
+            else
+            {
+                mutiplayerSynthesizeParticipationInfoDictionary.Add(info.playerID, info);
+                onMutiplayerSynthesizeParticipationInfoChange?.Invoke(DataChangeType.Add, info);
+            }
+        }
+        public void RemoveMutiplayerSynthesizeParticipant(int playerID)
+        {
+            if (ContainsMutiplayerSynthesizeParticipant(playerID))
+            {
+                MutiplayerSynthesizeParticipationInfo info = mutiplayerSynthesizeParticipationInfoDictionary[playerID];
+                mutiplayerSynthesizeParticipationInfoDictionary.Remove(playerID);
+                onMutiplayerSynthesizeParticipationInfoChange?.Invoke(DataChangeType.Remove, info);
+            }
+        }
+    }
+}
