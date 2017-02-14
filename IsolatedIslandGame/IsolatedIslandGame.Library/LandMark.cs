c3b006255224f9c9c1bbb17711ff194a735a86e0
DSobscure <snbruce13@yahoo.com.tw>
2017-02-14 20:04:59 +0800

    quest

---
+++ IsolatedIslandGame/IsolatedIslandGame.Library/LandMark.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IsolatedIslandGame.Library
+{
+    public class Landmark
+    {
+        public int LandmarkID { get; private set; }
+        public string LandmarkName { get; private set; }
+
+    }
+}
