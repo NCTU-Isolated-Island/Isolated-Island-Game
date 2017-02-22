9023642ebbae8b186390fce887026d53be0fd1b1
Steven's MacBook <Cyric@CyricdeMacBook.local>
2017-02-22 20:40:55 +0800

    no message

--- UnityProject/Assets/Scripts/GameManager.cs
+++ UnityProject/Assets/Scripts/GameManager.cs
@@ -63,7 +63,7 @@ public class GameManager : MonoBehaviour
     public void Login()
     {
         //FacebookService.LoginWithFacbook();
-        UserManager.Instance.User.OperationManager.PlayerIDLogin(22, "TestServer");
+        UserManager.Instance.User.OperationManager.PlayerIDLogin(36, "TestServer");
     }
 
     void LoadItemGameObject()
