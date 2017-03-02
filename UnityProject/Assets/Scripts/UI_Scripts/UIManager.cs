1eddaa279382a7daa5a1eaa68744a828b58df9f9
Steven <karta1297963@gmail.com>
2017-03-02 17:46:22 +0800

    ItemID modify

--- UnityProject/Assets/Scripts/UI_Scripts/UIManager.cs
+++ UnityProject/Assets/Scripts/UI_Scripts/UIManager.cs
@@ -23,7 +23,7 @@ public class UIManager : MonoBehaviour
     // DEBUG
     void OnGUI()
     {
-        GUI.Label(new Rect(50, 50, 100, 100), (1 / Time.deltaTime).ToString());
+        //GUI.Label(new Rect(50, 50, 100, 100), (1 / Time.deltaTime).ToString());
     }
 
     void Update()
