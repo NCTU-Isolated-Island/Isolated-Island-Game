6e2aa26c7ea406216a2056175408f8a9b5d616a4
謙謙 <alan0201tw@gmail.com>
2017-03-02 23:18:23 +0800

    Lin Bai Bu Gan La

--- UnityProject/Assets/Scripts/UI_Scripts/InventoryScripts/ItemInfoDetailPanel.cs
+++ UnityProject/Assets/Scripts/UI_Scripts/InventoryScripts/ItemInfoDetailPanel.cs
@@ -1,4 +1,5 @@
 ﻿using IsolatedIslandGame.Library.Items;
+using IsolatedIslandGame.Protocol;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,6 +7,7 @@ public class ItemInfoDetailPanel : MonoBehaviour
 {
     public InventoryItemInfo ItemInfo { get; private set; }
     private Image itemImage;
+	private Image itemGroupIcon;
     private Text itemNameText;
     private Text itemCountText;
     private Text itemDescriptionText;
@@ -20,6 +22,8 @@ public class ItemInfoDetailPanel : MonoBehaviour
         itemNameText = transform.Find("ItemNameText").GetComponent<Text>();
         itemCountText = transform.Find("ItemCountText").GetComponent<Text>();
         itemDescriptionText = transform.Find("ItemDescriptionText").GetComponent<Text>();
+
+		itemGroupIcon = transform.Find ("ItemGroupIcon").GetComponent<Image> ();
 
         //synthesizeButton = transform.Find("SynthesizeButton").GetComponent<Button>();
         //decorateVesselButton = transform.Find("DecorateVesselButton").GetComponent<Button>();
@@ -39,9 +43,26 @@ public class ItemInfoDetailPanel : MonoBehaviour
         itemNameText.text = ItemInfo.Item.ItemName;
         itemCountText.text = string.Format("x{0}", ItemInfo.Count);
         itemDescriptionText.text = ItemInfo.Item.Description;
+
         if(ItemInfo.Item is IsolatedIslandGame.Library.Items.Material)
         {
             itemDescriptionText.text += string.Format("\n素材分數： {0}", (ItemInfo.Item as IsolatedIslandGame.Library.Items.Material).Score);
+			switch ((ItemInfo.Item as IsolatedIslandGame.Library.Items.Material).GroupType) {
+			case GroupType.Animal:
+				itemGroupIcon.sprite = Resources.Load<Sprite> ("GroupIcon/animal");
+				break;
+			case GroupType.Businessman:
+				itemGroupIcon.sprite = Resources.Load<Sprite> ("GroupIcon/businessman");
+				break;
+			case GroupType.Farmer:
+				itemGroupIcon.sprite = Resources.Load<Sprite> ("GroupIcon/farmer");
+				break;
+			default:
+				Destroy (itemGroupIcon.gameObject);
+				break;
+			}
+
+			itemDescriptionText.text += string.Format("\n素材等級： {0}", (ItemInfo.Item as IsolatedIslandGame.Library.Items.Material).Level);
         }
     }
 }
