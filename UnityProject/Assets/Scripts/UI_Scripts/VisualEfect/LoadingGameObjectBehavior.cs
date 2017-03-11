7113703afd99a5b4b40d523f7cc33ebb1593fd2c
Alan Shih <alan0201tw@gmail.com>
2017-03-11 16:49:22 +0800

    Fix Lag by Quality Setting

---
+++ UnityProject/Assets/Scripts/UI_Scripts/VisualEfect/LoadingGameObjectBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingGameObjectBehavior : MonoBehaviour {
+
+    [SerializeField]
+    private Image upperCircle;
+    [SerializeField]
+    private Image lowerCircle;
+
+    private float theta;
+
+    void Update()
+    {
+        upperCircle.fillAmount = Mathf.Cos(theta);
+        lowerCircle.fillAmount = Mathf.Cos(2 * theta);
+
+        theta += Time.deltaTime;
+    }
+	
+}
