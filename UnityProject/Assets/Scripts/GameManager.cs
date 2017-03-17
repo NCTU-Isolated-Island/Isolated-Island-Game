d2c515078087a1a9784aca8d4506f14cefaa86ed
DESKTOP-6J14KSJ\AlanShih <alan0201tw@gmail.com>
2017-03-17 17:26:22 +0800

    Add Collider to 3D models and fix login scene text , add resources.load of ingredients

--- UnityProject/Assets/Scripts/GameManager.cs
+++ UnityProject/Assets/Scripts/GameManager.cs
@@ -62,12 +62,16 @@ public class GameManager : MonoBehaviour
 
     void LoadItemGameObject()
     {
-		elementModels.Add(1005, Resources.Load("Ingredients/" + "stone") as GameObject);
+        elementModels.Add(   1, Resources.Load("Ingredients/" + "trash") as GameObject);
+
+        elementModels.Add(1005, Resources.Load("Ingredients/" + "stone") as GameObject);
 		elementModels.Add(1006, Resources.Load("Ingredients/" + "Wheat") as GameObject);
 		elementModels.Add(1007, Resources.Load("Ingredients/" + "Egg") as GameObject);
 		elementModels.Add(1008, Resources.Load("Ingredients/" + "CoffeeBean") as GameObject);
+        elementModels.Add(1009, Resources.Load("Ingredients/" + "pineapple") as GameObject);
+        elementModels.Add(1010, Resources.Load("Ingredients/" + "apple") as GameObject);
 
-		elementModels.Add(1013, Resources.Load("Ingredients/" + "fire") as GameObject);
+        elementModels.Add(1013, Resources.Load("Ingredients/" + "fire") as GameObject);
 
 		elementModels.Add(1016, Resources.Load("Ingredients/" + "coal") as GameObject);
 
@@ -79,28 +83,34 @@ public class GameManager : MonoBehaviour
 		elementModels.Add(1022, Resources.Load("Ingredients/" + "fish") as GameObject);
 		elementModels.Add(1023, Resources.Load("Ingredients/" + "Sponge") as GameObject);
 
-		elementModels.Add(2006, Resources.Load("Ingredients/" + "milk") as GameObject);
+        elementModels.Add(2002, Resources.Load("Ingredients/" + "Ball") as GameObject);
+        elementModels.Add(2005, Resources.Load("Ingredients/" + "StoneMill") as GameObject);
+        elementModels.Add(2006, Resources.Load("Ingredients/" + "milk") as GameObject);
+        elementModels.Add(2007, Resources.Load("Ingredients/" + "badminton") as GameObject);
+        elementModels.Add(2008, Resources.Load("Ingredients/" + "knife") as GameObject);
+        elementModels.Add(2009, Resources.Load("Ingredients/" + "Sugar") as GameObject);
 
-		elementModels.Add(2008, Resources.Load("Ingredients/" + "knife") as GameObject);
-
-		elementModels.Add(2011, Resources.Load("Ingredients/" + "pigment") as GameObject);
-
-		elementModels.Add(2013, Resources.Load("Ingredients/" + "Flour") as GameObject);
+        elementModels.Add(2011, Resources.Load("Ingredients/" + "pigment") as GameObject);
+        elementModels.Add(2012, Resources.Load("Ingredients/" + "Pot") as GameObject);
+        elementModels.Add(2013, Resources.Load("Ingredients/" + "Flour") as GameObject);
 		elementModels.Add(2014, Resources.Load("Ingredients/" + "pencil") as GameObject);
 
 		elementModels.Add(2016, Resources.Load("Ingredients/" + "electricity") as GameObject);
 		elementModels.Add(2017, Resources.Load("Ingredients/" + "Propeller") as GameObject);
 
 		elementModels.Add(2019, Resources.Load("Ingredients/" + "gold") as GameObject);
+        elementModels.Add(2020, Resources.Load("Ingredients/" + "Candle") as GameObject);
 
-		elementModels.Add(3002, Resources.Load("Ingredients/" + "steel") as GameObject);
+        elementModels.Add(3001, Resources.Load("Ingredients/" + "Soup") as GameObject);
+        elementModels.Add(3002, Resources.Load("Ingredients/" + "steel") as GameObject);
 		elementModels.Add(3003, Resources.Load("Ingredients/" + "Chip") as GameObject);
 
 		elementModels.Add(3005, Resources.Load("Ingredients/" + "Pillow") as GameObject);
 
 		elementModels.Add(3008, Resources.Load("Ingredients/" + "Latte") as GameObject);
-
-		elementModels.Add(3012, Resources.Load("Ingredients/" + "guitar") as GameObject);
+        elementModels.Add(3009, Resources.Load("Ingredients/" + "Coke") as GameObject);
+        elementModels.Add(3011, Resources.Load("Ingredients/" + "Painter") as GameObject);
+        elementModels.Add(3012, Resources.Load("Ingredients/" + "guitar") as GameObject);
 
 		elementModels.Add(3014, Resources.Load("Ingredients/" + "small_light") as GameObject);
 
@@ -108,18 +118,24 @@ public class GameManager : MonoBehaviour
 		elementModels.Add(3018, Resources.Load("Ingredients/" + "take_copter_1") as GameObject);
 		elementModels.Add(3019, Resources.Load("Ingredients/" + "Bread") as GameObject);
 
-		elementModels.Add(4006, Resources.Load("Ingredients/" + "Motor") as GameObject);
+        elementModels.Add(4004, Resources.Load("Ingredients/" + "badminton_racket") as GameObject);
+        elementModels.Add(4006, Resources.Load("Ingredients/" + "Motor") as GameObject);
 
 		elementModels.Add(4009, Resources.Load("Ingredients/" + "ShineMood") as GameObject);
+        elementModels.Add(4010, Resources.Load("Ingredients/" + "black_horse") as GameObject);
 
-		elementModels.Add(4013, Resources.Load("Ingredients/" + "imac") as GameObject);
+        elementModels.Add(4013, Resources.Load("Ingredients/" + "imac") as GameObject);
 
 
 		elementModels.Add(4016, Resources.Load("Ingredients/" + "Fries") as GameObject);
 
 
-
-
+        // check
+        foreach (var element in elementModels)
+        {
+            if (element.Value == null)
+                print(element.Key);
+        }
 
 
 	
