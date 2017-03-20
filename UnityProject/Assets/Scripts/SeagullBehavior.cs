62a335378d6a0dde2668c16d6e4799d52e7d1d0e
DESKTOP-6J14KSJ\AlanShih <alan0201tw@gmail.com>
2017-03-20 21:44:17 +0800

    Update login scene and beautify script

--- UnityProject/Assets/Scripts/SeagullBehavior.cs
+++ UnityProject/Assets/Scripts/SeagullBehavior.cs
@@ -2,46 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SeagullBehavior : MonoBehaviour {
+public class SeagullBehavior : MonoBehaviour
+{
+    private Vector3 moveDirection;
+    private float lifeTime;
 
-	private Vector3 moveDirection;
-	private float lifeTime;
+    void Awake()
+    {
+        lifeTime = 120f;
 
-	void Awake()
-	{
-		lifeTime = 120f;
+        Vector2 unitCircle = Random.insideUnitCircle;
+        Vector3 tempFlyDirection = new Vector3(unitCircle.x, 0f, unitCircle.y);
 
-		Vector2 unitCircle = Random.insideUnitCircle;
-		Vector3 tempFlyDirection = new Vector3(unitCircle.x, 0f, unitCircle.y);
+        Vector3 spawnPosition = GameManager.Instance.PlayerGameObject.transform.position - tempFlyDirection * 50f
+            + Vector3.up * 7f;
+        moveDirection = Quaternion.Euler(0f, Random.Range(-30f, 30f), 0f) * tempFlyDirection;
 
-		Vector3 spawnPosition = GameManager.Instance.PlayerGameObject.transform.position - tempFlyDirection * 50f
-			+ Vector3.up * 7f;
-		moveDirection = Quaternion.Euler(0f,Random.Range(-30f,30f),0f) * tempFlyDirection;
+        transform.position = spawnPosition;
+        transform.rotation = Quaternion.LookRotation(moveDirection * -1);
+    }
 
-		transform.position = spawnPosition;
-		transform.rotation = Quaternion.LookRotation(moveDirection * -1);
-	}
+    void Update()
+    {
+        Move();
 
-	void Start () {
-		
+        lifeTime -= Time.deltaTime;
 
-	}
+        if (lifeTime <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
 
-
-	
-	void Update () {
-		Move();
-
-		lifeTime -= Time.deltaTime;
-
-		if(lifeTime <= 0)
-		{
-			Destroy(gameObject);
-		}
-	}
-
-	void Move()
-	{
-		transform.position += moveDirection * Time.deltaTime * 6.5f;
-	}
+    void Move()
+    {
+        transform.position += moveDirection * Time.deltaTime * 6.5f;
+    }
 }
--- UnityProject/Assets/Scripts/SeagullGenerator.cs
+++ UnityProject/Assets/Scripts/SeagullGenerator.cs
@@ -2,42 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SeagullGenerator : MonoBehaviour {
-
+public class SeagullGenerator : MonoBehaviour
+{
     public static SeagullGenerator Instance { get; private set; }
-
 
     [SerializeField]
     private GameObject seagullGameObject;
-	[SerializeField]
-	private Transform SeagullsFolder;
+    [SerializeField]
+    private Transform SeagullsFolder;
 
     private void Awake()
     {
         if (Instance == null)
             Instance = this;
-
     }
 
     private void Start()
     {
-		
-		InvokeRepeating("Generate",Random.Range(10,30),Random.Range(45,60));
-        
+        InvokeRepeating("Generate", Random.Range(10, 30), Random.Range(45, 60));
     }
 
-	void Update()
-	{
-		
-	}
-
-	void Generate()
-	{
-		if(GameManager.Instance.PlayerGameObject == null)
-			return;
-
-
-		Instantiate(seagullGameObject,SeagullsFolder);
-	}
+    void Generate()
+    {
+        if (GameManager.Instance.PlayerGameObject == null)
+            return;
+        Instantiate(seagullGameObject, SeagullsFolder);
+    }
 
 }
